d24fe5f0e5a364e9a4c1d47c27f248b59e9b91b8
dyy11-laktosa <hady4.1994@gmail.com>
2021-03-03 09:14:46 +0700

    latihan dan tugas praktikum ke 1 selesai

---
+++ praktikumtugas1/latihan1a.php
@@ -0,0 +1,16 @@
+<?php
+/*
+    Hady Ismanto Rachmat
+    203040134
+    Jumat,13.00
+*/
+?>
+
+<?php
+
+for ($i = 1; $i <= 3; $i++) {
+    for ($j = 1; $j <= 3; $j++) {
+        echo "Ini perulangan ke ($i,$j) <br>";
+    }
+}
+?>
