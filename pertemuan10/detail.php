430ee48088f3e7847fa1e3b1e9c5135c763f31e1
dyy11-laktosa <hady4.1994@gmail.com>
2021-05-07 07:34:59 +0700

    menyelesaikan tugas pertemuan 10

---
+++ pertemuan10/detail.php
@@ -0,0 +1,47 @@
+<?php
+/*
+    Hady Ismanto Rachmat
+    203040134
+    https://github.com/dy11-laktosa/pw2021_203040134
+    Pertemuan 10 
+    Mempelajari mengenai koneksi DB
+*/
+?>
+
+<?php
+require 'functions.php';
+
+// ambil data dari URL 
+$id = $_GET['id'];
+
+// query mahasiswa berdasarkan id
+$m = query("SELECT * FROM mahasiswa WHERE id = $id");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail mahasiswa</title>
+</head>
+
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+    <li><img src="img/<?= $m['gambar']; ?>"></li>
+    <li>NRP : <?= $m['nrp']; ?></li>
+    <li>Nama : <?= $m['nama']; ?></li>
+    <li>Email : <?= $m['email']; ?></li>
+    <li>Jurusan : <?= $m['jurusan']; ?></li>
+    <li><a href=""> ubah </a> | <a href=""> hapus</a> </li>
+    <li><a href="latihan3.php">Kembali ke daftar mahasiswa</a></li>
+  </ul>
+
+
+</body>
+
+</html>
